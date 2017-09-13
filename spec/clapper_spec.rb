34195423a6134baf259581be232413b1bb85ec58
John Simoni <email": null>
2017-09-13 11:56:16 -0700

    creates a new file if tests.csv doesn't exist

--- spec/clapper_spec.rb
+++ spec/clapper_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe Clapper do
   it "has a version number" do
     expect(Clapper::VERSION).to be nil
   end
+
+  describe 'Clapper.openFile' do
+    it "creates a new file if tests.csv doesn't exist" do
+      if File.exist?("tests.csv")
+        `rm tests.csv`
+      end
+      Clapper.openFile
+      expect(File.exist?("tests.csv")).to be true
+    end
+  end
 end
