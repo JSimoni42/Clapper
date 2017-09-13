8c9b8914720d7a6fdc0364a1bfc1ba13f2c5973d
Eliza Brine & John Simoni <pair+ebrine.jsimoni42@devbootcamp.com>
2017-09-12 18:26:44 -0700

    Removed comments and refactored code

--- lib/clapper.rb
+++ lib/clapper.rb
@@ -7,30 +7,17 @@ module Clapper
   end
 
   def self.compare(example_list)
-    # output = AudioPlayback::Device::Output.gets
     output = AudioPlayback::Device::Output.by_id(1)
-    # p "*" * 100
-    # p output
     options = {
       :channels => [0,1],
       :latency => 1,
       :output_device => output,
       :duration => 5
     }
-
-    if !File.exist?("tests.txt")
-      f = File.new("tests.txt", "w+")
-    else
-      f = File.open("tests.txt", "r+")
-    end
+    f = Clapper.openFile
     old_examples = f.readlines
     f.close
-    parsed_examples = {}
-    i = 0
-    while i+1 < old_examples.length
-      parsed_examples[old_examples[i].chomp] = old_examples[i+1].chomp.to_sym
-      i += 2
-    end
+    parsed_examples = Clapper.parse_examples(old_examples)
     will_clap = false
     f = File.open("tests.txt", "w+")
     example_list.each do |example|
@@ -48,5 +35,25 @@ module Clapper
       playback = AudioPlayback.play("audio/Clapping.wav", options)
       playback.block
     end
+  end
+
+  private
+
+  def self.openFile
+    if !File.exist?("tests.txt")
+      File.new("tests.txt", "w+")
+    else
+      File.open("tests.txt", "r+")
+    end
+  end
+
+  def self.parse_examples(old_examples)
+    parsed_examples = {}
+    i = 0
+    while i+1 < old_examples.length
+      parsed_examples[old_examples[i].chomp] = old_examples[i+1].chomp.to_sym
+      i += 2
+    end
+    parsed_examples
   end
 end
